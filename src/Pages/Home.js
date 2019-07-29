3eb9571104cbcb79d92aa3829f85e6300d5e7994
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-07-29 15:20:52 -0400

    added moment

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,11 +4,11 @@ class Home extends Component {
   render() {
     return (
       <>
-        <section>
+        <section className="top-section-flex">
           <header>
             <h1 className="header">What a Time to be Funny..</h1>
           </header>
-          <h1>Clock goes Here</h1>
+          <h1 className="clock">Clock goes Here</h1>
         </section>
         <section className="joke-section">
           <h1 className="joke-heading">This is a joke I think..</h1>
