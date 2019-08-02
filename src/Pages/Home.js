a60a7dc9733b634759ce5207b9572a69d7d6b857
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-08-02 14:00:56 -0400

    updated api

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
 import moment from 'moment'
+import axios from 'axios'
 
 class Home extends Component {
+  state = {
+    joke: ''
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://sv443.net/jokeapi/category/Any', {
+        headers: {
+          'content-type': 'application/json',
+          Accepts: 'application/json'
+        }
+      })
+      .then(resp => {
+        console.log(resp)
+        resp.data.joke = this.joke
+      })
+  }
+
+  getMeAJoke = () => {
+    console.log('button clicked')
+    axios.get('https://sv443.net/jokeapi/category/Any').then(resp => {
+      console.log(resp)
+    })
+  }
+
   render() {
     return (
       <>
@@ -16,8 +42,10 @@ class Home extends Component {
         <section className="joke-section">
           <h1 className="joke-heading">This is a joke I think..</h1>
           <hr />
-          <p className="joke">Joke goes here</p>
-          <button className="joke-button"> New Joke Please! </button>
+          <p className="joke">{this.joke}</p>
+          <button onClick={this.getMeAJoke} className="joke-button">
+            New Joke Please!
+          </button>
         </section>
       </>
     )
